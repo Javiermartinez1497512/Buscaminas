f9fde01b67e90cfdb96f39f3e6f30be4b2115cda
1497512 <javier.martinezab@e-campus.uab.cat>
2020-10-12 15:01:08 +0200

    Pruebas Travis

--- src/test/java/Buscaminas/LibraryTest.java
+++ src/test/java/Buscaminas/LibraryTest.java
@@ -11,4 +11,8 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    @Test
+    public void test() {
+    	assertTrue(true);
+    }
 }
