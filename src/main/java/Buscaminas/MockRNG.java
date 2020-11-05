ea9628a4035de002d10822bf9d7f99367c62e5a3
Javiermartinez1497512 <72439858+Javiermartinez1497512@users.noreply.github.com>
2020-11-05 11:32:00 +0100

    Update MockRNG.java

--- src/main/java/Buscaminas/MockRNG.java
+++ src/main/java/Buscaminas/MockRNG.java
@@ -2,18 +2,16 @@ package Buscaminas;
 
 
 public class MockRNG implements RNG{
-	int Config;
-	
-	int v[][]= {{0,0},{0,-1},{-1,0},{8,7},{7,8},{0,8},{8,0},{-1,7},{7,-1},{0,1},{1,7},{0,7},{7,0},{7,1},{6,7},{7,7}};
+	private int config;
+	private int v[][]= {{0,0},{0,-1},{-1,0},{8,7},{7,8},{0,8},{8,0},{-1,7},{7,-1},{0,1},{1,7},{0,7},{7,0},{7,1},{6,7},{7,7}};
 	
 	public void setConfig(int c){
-		Config=c;		
+		this.config=c;		
 	}
 	
 	public int[] getRandomNumber() {
-		
-		int V[] = v[Config];
-		Config++;		
+		int V[] = v[this.config];
+		this.config++;		
 		return V;
 	}
 
